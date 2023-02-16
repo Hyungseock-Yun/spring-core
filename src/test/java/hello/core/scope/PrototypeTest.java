fffeb7d8199cffad38f4ca2b8c0512fde412cb7f
Hyungseock-Yun <bagzein@gmail.com>
2023-02-17 03:39:30 +0900

    - 프로토타입 스코프 테스트 - 싱글톤 스코프 테스트

--- src/test/java/hello/core/scope/PrototypeTest.java
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -1,5 +1,6 @@
 package hello.core.scope;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -7,12 +8,27 @@ import org.springframework.context.annotation.Scope;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class PrototypeTest {
 
+  // 프로토타입 빈은 스프링 컨테이너에서 빈을 조회할 때 생성되고, 초기화 메서드도 실행.
+  // 2번 조회했으므로 각자 다른 스프링 빈이 생성되고, 초기화도 2번 실행됨.
+  // 프로토타입 빈의 경우 생성, 의존관계 주입 그리고 초기화 까지만 관여하여 @PreDestroy같은 종료 메서드가 실행되지 않음
   @Test
   void prototypeBeanFind() {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+    System.out.println("find prototypeBean1");
+    PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+    System.out.println("find prototypeBean2");
+    PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+    System.out.println("prototypeBean1 = " + prototypeBean1);
+    System.out.println("prototypeBean2 = " + prototypeBean2);
+    assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
 
+    ac.close();
+//    prototypeBean1.destroy();
+//    prototypeBean2.destroy();
   }
 
   @Scope("prototype")
--- src/test/java/hello/core/scope/SingletonTest.java
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -10,6 +10,8 @@ import javax.annotation.PreDestroy;
 
 public class SingletonTest {
 
+  // 싱글톤 빈의 경우, 스프링 컨테이너 생성 시점에 초기화 메서드가 실행됨.
+  // 같은 객체이므로 init 1회 실행 후, destroy.
   @Test
   void singletonBeanFind() {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
